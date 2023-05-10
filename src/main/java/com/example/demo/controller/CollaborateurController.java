d8bae26944eeef031eeab36648aace591a6bf9a4
Nohayla TANAJYT <103898244+nohaylaTanajyt1@users.noreply.github.com>
2023-05-10 16:51:55 +0200

    Update CollaborateurController.java

--- src/main/java/com/example/demo/controller/CollaborateurController.java
+++ src/main/java/com/example/demo/controller/CollaborateurController.java
@@ -1,13 +1,39 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repository.*;
+
+import java.util.List;
+import com.example.demo.model.*;
 
 @RestController
 public class CollaborateurController {
 	@Autowired
 	CollaborateurRepository collaborateurRepository;
+
+	@GetMapping(value="/all")
+	public List<Collaborateur> all() {
+		return collaborateurRepository.findAll();
+	}
+	
+	@GetMapping(value="/{codecollaborateur}")
+	public Collaborateur getcollaborateur(@PathVariable(name="codecollab") int codecollab) {
+		return collaborateurRepository.findById(codecollab).get();
+	}
+	@PostMapping(value="/new")
+	public Collaborateur newcollaborateur (@RequestBody Collaborateur collaborateur) {
+		return collaborateurRepository.save(collaborateur);
+	}
+	@PutMapping(value="/update")
+	public Collaborateur updatecollaborateur(@RequestBody Collaborateur collaborateur) {
+		return collaborateurRepository.save(collaborateur);
+	}
+	@DeleteMapping(value="/delete/{codecollab}")
+	public void deletecollaborateur (@PathVariable(name="codecollab") int codecollab) {
+		collaborateurRepository.deleteById(codecollab);
+	}
 
 }
